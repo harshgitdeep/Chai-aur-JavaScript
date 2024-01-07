7b33c27f4092ce8819bdb4d367a205810d9eb1ee
harshgitdeep <harshzone3@gmail.com>
2024-01-07 18:55:47 +0530

    Datatypes

---
+++ Lecture 7 8 Comparison of datatypes/DataTypes Summary.js
@@ -0,0 +1,28 @@
+/* JavaScript is dynamically typed. This means that variable types are determined at runtime, allowing for more flexibility as variables can hold different types of data throughout the code. Unlike statically typed languages where the type of a variable is explicitly declared and enforced, in JavaScript, types are associated with values, not variables, and can change during execution.
+
+----------------- DATA TYPES in JavaScript ------------------
+
+
+In JavaScript, data types are classified into two main categories:
+
+Primitive Data Types:
+
+1. Number: Represents both integer and floating-point numbers.
+2. String: Represents a sequence of characters, such as text.
+3. Boolean: Represents a logical value, either true or false.
+4. Undefined: Represents a variable that has been declared but hasn't been assigned a value.
+5. Null: Represents an intentional absence of any value.
+6. Symbol: Represents a unique and immutable value used as an identifier for object properties.
+7. BigInt
+
+
+Non-primitive Data Type:
+
+1. Object: Represents a collection of key-value pairs, where values can be of any data type, including other objects. Object Literal: An unordered collection of key-value pairs wrapped in curly braces {}. It represents complex data structures and can contain properties and methods.
+
+2. Arrays: Ordered collections of values enclosed in square brackets []. Arrays hold multiple values accessible via numeric indices and have built-in methods for manipulation.
+
+3. Functions: Blocks of reusable code that can be invoked by name. Functions are treated as objects in JavaScript, allowing them to be assigned to variables, passed as arguments, and returned from other functions.
+
+
+*/
